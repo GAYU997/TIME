4fe66e2da0ba6fdf0dfdb0a380f113c3ff0abe20
Gayathri.v <gayu9158@gmail.com>
2020-07-09 17:16:28 +0530

    commit

---
+++ app/controller/postcontroller.js
@@ -0,0 +1,17 @@
+var app =angular.module("insert",[]);
+app.controller('myController', function ($scope, $http) {  
+    $scope.insert= function() {
+      $http.post(
+             "api/insert_data.php",{
+              'name': $scope.name,
+               'email': $scope.email,
+                'age': $scope.age,
+                'time':$scope.time,
+            }).then (function(data){
+            $scope.name = null;
+            $scope.email = null;
+            $scope.age=null;
+            $scope.time=null;
+            });
+    }
+    }); 
