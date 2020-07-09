acf2f209a9eece4774dee2f9a031600ecdf262f4
GAYU997 <67303008+GAYU997@users.noreply.github.com>
2020-07-09 17:19:56 +0530

    commit

---
+++ api/insert_data.php
@@ -0,0 +1,24 @@
+<?php
+$mysqli = mysqli_connect("localhost","root","","angular_insert");
+ $info = (array)json_decode(file_get_contents("php://input")); 
+if(count($info) > 0)
+{
+   
+    $name = mysqli_real_escape_string($mysqli, $info['name']);
+    $email = mysqli_real_escape_string($mysqli, $info['email']);
+    $age = mysqli_real_escape_string($mysqli, $info['age']);
+    $time = mysqli_real_escape_string($mysqli, $info['time']);
+
+    $query = "INSERT INTO emp_info( name , email ,age , time) VALUES('$name','$email','$age','$time')"; 
+     
+    if(mysqli_query($mysqli,$query))
+    {
+        echo "Data inserted successfully";
+    }
+    else
+    {
+       echo "Failed";
+    }
+}
+
+?>
